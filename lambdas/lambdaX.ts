04472303a4ce74fd5276e46c12abef34630d4d86
BruceWan1021 <brucewan1021@foxmail.com>
2025-05-02 12:31:31 +0100

    find peoblem in lambdaX forSns and changed it into SQS

--- lambdas/lambdaX.ts
+++ lambdas/lambdaX.ts
@@ -2,25 +2,32 @@ import { Handler } from "aws-lambda";
 import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 
 const client = new SQSClient({ region: process.env.REGION });
-export const handler: Handler = async (event, context) => {
+
+export const handler: Handler = async (event) => {
   try {
     console.log("Event: ", JSON.stringify(event));
 
     for (const record of event.Records) {
-      const snsMessage = JSON.parse(record.Sns.Message);
-      if (!snsMessage.email) {
+      const message = JSON.parse(record.body); 
+
+      const country = message.address?.country;
+      const email = message.email;
+
+      if (!email && (country === "Ireland" || country === "China")) {
         console.log("Missing email. Forwarding to QueueB...");
+
         await client.send(
           new SendMessageCommand({
             QueueUrl: process.env.QUEUE_B_URL!,
-            MessageBody: JSON.stringify(snsMessage),
+            MessageBody: JSON.stringify(message),
           })
         );
       } else {
-        console.log("Email exists. Ignoring message.");
+        console.log("Email exists or country not allowed. Ignoring.");
       }
     }
   } catch (error: any) {
+    console.error("Error:", error);
     throw new Error(JSON.stringify(error));
   }
 };
